58afe5dd11b8964a77da8ca5fb27f6352e8997d6
mayookha-0405 <mayookha040504@gmail.com>
2025-02-05 14:46:42 +0530

    2.1

---
+++ part2/2.1/src/App.jsx
@@ -0,0 +1,51 @@
+const Header = (props) => <h1>{props.course}</h1>;
+
+const Content = (props) => (
+  <div>
+    {props.parts.map((part) => (
+      <Part key={part.id} part={part} />
+    ))}
+  </div>
+);
+
+const Part = (props) => (
+  <p>
+    {props.part.name} {props.part.exercises}
+  </p>
+);
+
+const Course = ({ course }) => (
+  <div>
+    <Header course={course.name} />
+    <Content parts={course.parts} />
+  </div>
+);
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+        id: 1,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+        id: 2,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+        id: 3,
+      },
+      // You can add or remove parts here without breaking the application
+    ],
+  };
+
+  return <Course course={course} />;
+};
+
+export default App;
