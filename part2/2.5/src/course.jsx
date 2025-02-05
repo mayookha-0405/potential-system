60500f37f93543191dbab1462e149fd7af56929e
mayookha-0405 <mayookha040504@gmail.com>
2025-02-05 15:33:44 +0530

    2.5

---
+++ part2/2.5/src/course.jsx
@@ -0,0 +1,31 @@
+// Course.js
+const Header = ({ course }) => <h1>{course}</h1>;
+
+const Content = ({ parts }) => (
+  <div>
+    {parts.map((part) => (
+      <Part key={part.id} part={part} />
+    ))}
+  </div>
+);
+
+const Part = ({ part }) => (
+  <p>
+    {part.name} {part.exercises}
+  </p>
+);
+
+const Total = ({ parts }) => {
+  const total = parts.reduce((sum, part) => sum + part.exercises, 0);
+  return <p><strong>Total of {total} exercises</strong></p>;
+};
+
+const Course = ({ course }) => (
+  <div>
+    <Header course={course.name} />
+    <Content parts={course.parts} />
+    <Total parts={course.parts} />
+  </div>
+);
+
+export default Course;
